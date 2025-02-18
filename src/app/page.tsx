843cccdd81d4c5237f0bb5495060ec533a918596
Luís Miguel <miguelpalves01@gmail.com>
2025-02-18 13:55:04 -0300

    core: add eslint sort plugin and prettier tailwind plugin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 const HomePage = () => {
   return (
     <div className="p-5 border border-red-500 rounded-xl">
-      <h1 className="text-red-500">Home Page</h1>
+      <h1 className="text-red-500 p-5">Home Page</h1>
 
       </div>
   );
