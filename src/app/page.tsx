1c1aac275ce123549e3035d97963509965259f84
Luís Miguel <miguelpalves01@gmail.com>
2025-02-18 13:43:15 -0300

    chore: update global css variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 const HomePage = () => {
   return (
     <div className="p-5 border border-red-500 rounded-xl">
       <h1 className="text-red-500">Home Page</h1>
+
       </div>
   );
 }
