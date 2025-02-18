8bc567b417b8984769422064bb4408d6fcd66f37
Luís Miguel <miguelpalves01@gmail.com>
2025-02-18 13:43:53 -0300

    fix: useless imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-
 const HomePage = () => {
   return (
     <div className="p-5 border border-red-500 rounded-xl">
